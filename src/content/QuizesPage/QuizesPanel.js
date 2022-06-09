7786ddd307ed40f7cf16632eec81bc87bf2c5bd4
Jose Luis Rodriguez G <joseluisrg@gmail.com>
2022-06-08 22:27:15 -0500

    feat(tutorial): fixed bugs

--- src/content/QuizesPage/QuizesPanel.js
+++ src/content/QuizesPage/QuizesPanel.js
@@ -13,20 +13,21 @@ const QuizesPanel = ({
   };
 
   return (
-    <div id={questionId} className="bx--row quizes-page__panel">
+    <div id={questionId + 'p'} className="bx--row quizes-page__panel">
       <div className="bx--col-lg-8 bx--col-lg-8 quizes-page__question">
         <div className="quizes-page__question_small">Pregunta</div>
         {questionText}
       </div>
       <div className="bx--col-lg-8 bx--col-lg-8 quizes-page__option">
         <RadioButtonGroup
+          id={questionId}
           legendText="Radio button heading"
           name="radio-button-group"
           onChange={handleOnChange}
-          defaultSelected="-1"
           className="quizes-page__option">
           {qOptions.map(qOption => (
             <RadioButton
+              id={qOption.id}
               labelText={qOption.text}
               value={qOption.id}
               key={qOption.id}
